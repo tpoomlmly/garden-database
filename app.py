3292c275a809907405ef685fd26dc9ac4ebfdfc4
tpoomlmly <Tommy@poolle.co.uk>
2018-06-18 09:53:46 +0100

    added database file

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, send_from_directory
 from flask_sslify import SSLify
+import sqlite3
 import os
 
 app = Flask(__name__)
 sslify = SSLify(app)
+db = sqlite3.connect('garden.db')
 
 
 @app.route("/")
@@ -26,9 +28,7 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/x-icon')
 
-# app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon.ico'))
 
-
-if __name__ == "__main__":        
+if __name__ == "__main__":
     context = ('server.crt', 'server.key')
     app.run(host='localhost', port='443', debug=False, ssl_context=context)
