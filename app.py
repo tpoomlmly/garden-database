a152622cf7fd072afb6ad393349d8abcde9b8ccf
tpoomlmly <Tommy@poolle.co.uk>
2018-10-08 21:52:00 +0100

    Added edit modals with working delete functionality

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime as dt
 import dbc
 import os
 
+# TODO use flash() to add invalid form detection including non-unique latin names
 app = Flask(__name__)
 sslify = SSLify(app=app, permanent=True)
 months = ["January", "February", "March", "April", "May", "June", "July",
@@ -19,36 +20,45 @@ def index():
 def clients():
     # TODO add plant linking support to the form and backend
     if request.method == "POST":
-        client = dbc.Client(request.form["name"])
-        dbc.insert_client(client)
-    return render_template("clients.html", c_data=dbc.load_sql_client_data())
+        if "delete" in request.form:
+            dbc.drop_client(request.form["id"])
+        else:
+            client = dbc.Client(request.form["name"])
+            dbc.insert_client(client)
+    return render_template("clients.html", data=dbc.load_sql_client_data())
 
 
 @app.route("/plants", methods=["GET", "POST"])
 def plants():
     # TODO add maintenance linking support to the form and backend
     # TODO add sensible way to handle blooming period (perhaps month selection again?)
-    # TODO use flash() to add invalid form detection including non-unique latin names
     if request.method == "POST":
-        plant = dbc.Plant(request.form["name"],
-                          request.form["latin-name"],
-                          request.form["blooming-period"])
-        dbc.insert_plant(plant)
-    return render_template("plants.html", p_data=dbc.load_sql_plant_data())
+        if "delete" in request.form:
+            dbc.drop_plant(request.form["id"])
+        else:
+            plant = dbc.Plant(request.form["name"],
+                              request.form["latin-name"],
+                              request.form["blooming-period"])
+            dbc.insert_plant(plant)
+    return render_template("plants.html", data=dbc.load_sql_plant_data())
 
 
 @app.route("/maintenance", methods=["GET", "POST"])
 def jobs():
     if request.method == "POST":
-        # & is the intersection operator. set() converts the dict_keyiterator and list to sets so that the
-        # intersection can be found of them. list() converts this back to a list which is sorted on the basis that
-        # each item in the new list is the name of a month.
-        active_months = sorted(list(set(request.form) & set(months)), key=dt_from_month)
-        job = dbc.Maintenance(request.form["name"],
-                              request.form["desc"],
-                              active_months)
-        dbc.insert_job(job)
-    return render_template("maintenance.html", j_data=dbc.load_sql_job_data(), month_list=months)
+        if "delete" in request.form:
+            print(request.form["id"])
+            dbc.drop_job(request.form["id"])
+        else:
+            # & is the intersection operator. set() converts the dict_keyiterator and list to sets so that the
+            # intersection can be found of them. list() converts this back to a list which is sorted on the basis that
+            # each item in the new list is the name of a month.
+            active_months = sorted(list(set(request.form) & set(months)), key=dt_from_month)
+            job = dbc.Maintenance(request.form["name"],
+                                  request.form["desc"],
+                                  active_months)
+            dbc.insert_job(job)
+    return render_template("maintenance.html", data=dbc.load_sql_job_data(), month_list=months)
 
 
 @app.route("/favicon.ico")
