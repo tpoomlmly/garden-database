a670be56ec4cb34118ca2dee3d01b240a3a587b6
tpoomlmly <Tommy@poolle.co.uk>
2018-11-12 16:53:26 +0000

    Add exception handling for imaginary months

--- sorting/__init__.py
+++ sorting/__init__.py
@@ -1,9 +1,13 @@
 from datetime import datetime as dt
+from werkzeug.exceptions import BadRequest
 
 
 def dt_from_month(month):
     """Converts the name of a month to a sortable datetime object.
 
-    Only works in english countries.
+    Only works with English months.
     """
-    return dt.strptime(month, '%B')
+    try:
+        return dt.strptime(month, '%B')
+    except ValueError:
+        raise  BadRequest
